e765cdba8cd893de1d30b42f6827840f65613365
JackLL <3400407358@qq.com>
2023-01-27 19:41:13 +0800

    初始存储

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Bar(props){
+    return (
+        <button onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Text(props){
+    return(
+        <div>
+            <div>
+                if(0===page){
+                    <p>aaa</p>
+                }
+                else if(1===page){
+                    <p>旧密码</p> 
+                    
+                }
+                else if(2===page){
+                    
+                }
+            </div>
+        </div>
+    );
+}
+
+class TopBars extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            page:0,
+        }
+    }
+    
+
+    render(){
+        return(
+            <div>
+                <div>
+                    <Bar
+                        value='基本资料'
+                    />
+                    <Bar
+                        value='修改密码'
+                    />
+                    <Bar
+                        value='其他'
+                    />
+                </div>
+                <div>
+                    if(0===page){
+                        <div>
+                            <p>aaa</p>
+                        </div>
+                    }
+                    else if(1===page){
+                        <p>旧密码</p> 
+                        
+                    }
+                    else if(2===page){
+                        
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<TopBars />);
